1ecf4c47fb6e59a10aecc523de76be1db9b85225
Nguyenvandinh100013 <128126311+Nguyenvandinh100013@users.noreply.github.com>
2025-03-23 16:55:53 +0700

    test_product list

---
+++ mobile/resources/locator/productList_locator.py
@@ -0,0 +1,7 @@
+class ProductList_Locator:
+    txt_product_type = '//android.widget.Button[@content-desc="_DYNAMIC_"]'
+    lbl_product_image = '//android.widget.ImageView[@content-desc="_DYNAMIC_"]'
+    # txt_raucu_product = '//android.widget.Button[@content-desc="Rau củ"]'
+    # txt_sinhto_product = '//android.widget.Button[@content-desc="Sinh tố"]'
+    # txt_thucphamtuoisong_product = '//android.widget.Button[@content-desc="Thực phẩm tươi sống"]'
+    
