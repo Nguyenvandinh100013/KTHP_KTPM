ee46c9b7502d04c5d16a93f5f69a6bee41b28ee5
Thai Van Phuc <phuc100662@donga.edu.vn>
2025-04-06 22:23:34 +0700

    testdocker

--- web/resources/config/config_web.py
+++ web/resources/config/config_web.py
@@ -1,3 +1,7 @@
+import os , sys
+web_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(web_root)
 class Config:
-    WEBDRIVER_PATH = "C:/webdrivers/chromedriver.exe"
+    # WEBDRIVER_PATH = "C:/webdrivers/chromedriver.exe"
+    WEBDRIVER_PATH = os.path.join(web_root, "driver", "chromedriver.exe")
     LOGIN_URL = "https://webbanhang-6.onrender.com/"
