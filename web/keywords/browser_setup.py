60ebb7bd7102cfefc38b4465c488fae2056948ff
Thai Van Phuc <phuc100662@donga.edu.vn>
2025-04-11 17:51:48 +0700

    test icd

--- web/keywords/browser_setup.py
+++ web/keywords/browser_setup.py
@@ -13,7 +13,6 @@ class BrowserSetup:
         if is_ci:
             options.add_argument('--headless')
             options.add_argument('--disable-gpu')
-            options.add_argument('--no-sandbox')
             options.add_argument('--window-size=1920,1080')
         else:
             options.add_experimental_option("detach", True)
