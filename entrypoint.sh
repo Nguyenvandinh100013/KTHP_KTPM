2b9d46a82bf7abf68b078902ea60dd546400b3cb
Thai Van Phuc <phuc100662@donga.edu.vn>
2025-04-20 19:25:54 +0700

    run android

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,16 @@
 # Chạy Appium
 appium --address 0.0.0.0 --port 4723 --log-level info &
 
-sleep 80
+# Đợi Appium khởi động
+until curl -s http://localhost:4723/wd/hub/status | grep -q '"status": 0'; do
+    echo "Waiting for Appium server to start..."
+    sleep 5
+done
+
+# Kết nối với Android container qua adb
 echo "Connecting to Android container..."
 adb connect android-container:5555
 adb devices
+
+# Chạy test với pytest
 pytest /app/tests/ --html=/report/test_report.html --self-contained-html
