50da6c97de1c859d2e18b8849a1158e4250ff787
Thai Van Phuc <phuc100662@donga.edu.vn>
2025-04-20 20:47:22 +0700

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Chạy Appium
-appium &
+appium
 
 # Kết nối với Android container qua adb
 echo "Connecting to Android container..."
 adb connect android-container:5555
 adb devices
-
+sleep 80
 # Chạy test với pytest
 pytest /app/tests/ --html=/report/test_report.html --self-contained-html
